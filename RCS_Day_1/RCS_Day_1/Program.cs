ced4f42a694d8fe11265788e42dfa6b08aa9543d
AlibabaTheThief <36454629+AlibabaTheThief@users.noreply.github.com>
2018-12-13 19:58:41 +0200

    First

---
+++ RCS_Day_1/RCS_Day_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCS_Day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet savu vārdu:");
+            string vards;
+            vards = Console.ReadLine();
+            Console.WriteLine("Sveiks, " + vards + "!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
