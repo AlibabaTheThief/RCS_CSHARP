9db20c7b4d9ea2248f1acff07f74d316fcb2629c
AlibabaTheThief <36454629+AlibabaTheThief@users.noreply.github.com>
2018-12-13 21:26:09 +0200

    Update Program.cs

--- RCS_Day_1/RCS_Day_1/Program.cs
+++ RCS_Day_1/RCS_Day_1/Program.cs
@@ -10,13 +10,71 @@ namespace RCS_Day_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ievadiet savu vārdu:");
-            string vards;
-            vards = Console.ReadLine();
-            Console.WriteLine("Sveiks, " + vards + "!");
+            Console.WriteLine("Ievadiet pirmo skaitli:");
+            String input = Console.ReadLine();
+            int x = Convert.ToInt32(input);
+            Console.WriteLine("Ievadiet otro skaitli:");
+            String input2 = Console.ReadLine();
+            int y = Convert.ToInt32(input2);
+            
+
+            int result = OtraisUzdevums( x, y);
+            Console.WriteLine(result);
+
+
             Console.ReadLine();
+        }
+
+        static void PirmaisUzdevums()
+        {
+            Console.WriteLine("Ievadiet savu vārdu:");
+            String vards = Console.ReadLine();
+            Console.WriteLine("Sveiks, " + vards + "!");
+
+            Console.ReadLine();
+        }
+
+        static int OtraisUzdevums(int x, int y)
+        {
+            Console.WriteLine("Ievadiet + lai saskaitītu vai - lai atņemtu:");
+            String input3 = Console.ReadLine();
+
+            if (input3 == "+")
+            {
+                return x + y;
+            }
+            else
+            {
+                return x - y;
+            }
 
 
+
+        }
+
+        static int Piemers()
+        {
+            return 4 + 3;
+        }
+
+        static int Piemers2(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Piemers3()
+        {
+            Console.WriteLine("Ievadiet kko:");
+            String ievade = Console.ReadLine();
+            int a = 5;
+            if ( ievade == "vii")
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
         }
     }
 }
